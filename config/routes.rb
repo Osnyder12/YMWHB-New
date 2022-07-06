6501cdccfdb2b77a9cfb9ecb71ff6f18a71b479c
Osnyder12 <snyderowen12@gmail.com>
2022-07-06 16:49:24 -0400

    navbar added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'homes#index'
+
   resources :power_rankings
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
