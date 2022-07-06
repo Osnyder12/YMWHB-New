483cd5db0ec162f516cba9248fac833b164ec83c
Osnyder12 <snyderowen12@gmail.com>
2022-07-06 17:36:42 -0400

    user scaffold added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
   root 'homes#index'
 
   resources :power_rankings
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
