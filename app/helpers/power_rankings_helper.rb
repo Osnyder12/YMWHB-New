f9342a97dfa1d2ca8b3f4c454831fa0fc995e412
Osnyder12 <snyderowen12@gmail.com>
2022-09-02 13:59:49 -0400

    working on column method for weeks

--- app/helpers/power_rankings_helper.rb
+++ app/helpers/power_rankings_helper.rb
@@ -2,4 +2,23 @@ module PowerRankingsHelper
   def edit_label(power_ranking)
     power_ranking.rankings.empty? ? 'Add Rankings' : 'Edit Rankings'
   end
+
+  def weeks_column(index:)
+    add = index + 1
+
+    if add == 4
+      add = 1
+      index = 0
+    elsif add == 10
+      add = 1
+      index = 0
+    else
+      "finish writing method"
+    end
+
+    start = index + add
+    end_col = start + 2
+
+    "col-start-#{start} col-end-#{end_col}"
+  end
 end
