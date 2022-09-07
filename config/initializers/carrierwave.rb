d9f13ac36f5a3728e33825a0e9b88c154f737bb6
Osnyder12 <snyderowen12@gmail.com>
2022-09-07 17:10:31 -0400

    removed some notes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,9 +8,6 @@ CarrierWave.configure do |config|
   }
   
   config.fog_directory  = 'ymwhb'
-  config.fog_public     = false                                                # optional, defaults to true
-  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
-  # For an application which utilizes multiple servers but does not need caches persisted across requests,
-  # uncomment the line :file instead of the default :storage.  Otherwise, it will use AWS as the temp cache store.
-  # config.cache_storage = :file
+  config.fog_public     = false
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
 end
