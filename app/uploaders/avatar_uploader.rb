21a9d105d301893281239c8c435edafd40fc280f
Osnyder12 <snyderowen12@gmail.com>
2022-09-07 09:13:33 -0400

    small update

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -11,7 +11,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     if Rails.env.production?
-      "#{Rails.root}/assets/images/#{model.id}"
+      "#{Rails.root}/assets/images/"
     else
       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
