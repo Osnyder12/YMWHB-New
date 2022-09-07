2eae87570a262956caec5b6ecc70414c8b1bb5bc
Osnyder12 <snyderowen12@gmail.com>
2022-09-07 19:25:17 -0400

    reuse store dir

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -15,13 +15,13 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  # def store_dir
-  #   if Rails.env.production?
-  #     "#{Rails.root}/public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  #   else
-  #     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  #   end
-  # end
+  def store_dir
+    if Rails.env.production?
+      "#{Rails.root}/public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    else
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
